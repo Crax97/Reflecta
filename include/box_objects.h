ffb52807154b39a36109230beb987e71188d1313
Crax97 <gsolimeno97@gmail.com>
2021-05-28 21:29:37 +0200

    Added Box Objects to repo

--- include/box_objects.h
+++ include/box_objects.h
@@ -2,23 +2,33 @@
 
 namespace Reflecta {
     class IntObject : public Object {
+    public:
 		std::string to_string() const override;
 		class MetaDescriptor* get_meta_descriptor() override;
+    int value() const;
     };
     class LongObject : public Object {
+    public:
 		std::string to_string() const override;
 		class MetaDescriptor* get_meta_descriptor() override;
+    long value() const;
     };
     class BoolObject : public Object {
+    public:
 		std::string to_string() const override;
 		class MetaDescriptor* get_meta_descriptor() override;
+    bool value() const;
     };
     class FloatObject : public Object {
+    public:
 		std::string to_string() const override;
 		class MetaDescriptor* get_meta_descriptor() override;
+    float value() const;
     };
     class DoubleObject : public Object {
+    public:
 		std::string to_string() const override;
 		class MetaDescriptor* get_meta_descriptor() override;
+    double value() const;
     };
 }
