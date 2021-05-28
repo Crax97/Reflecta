1a6cdbab61fa225c126fd11e3dd54a470ec12eb4
Crax97 <gsolimeno97@gmail.com>
2021-05-28 15:28:47 +0200

    Added tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,2 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-
-TEST_CASE("Testing basic object creation and destruction through MetaDescriptor's descriptor and destructors") {
-	
-}
---
+++ tests/tests.cpp
@@ -0,0 +1,60 @@
+#include "doctest.h"
+
+#include "object.h"
+#include "meta_descriptor.h"
+
+using namespace Reflecta;
+
+class MyTestClass : public Object {
+    
+    OBJECT_BASE()
+
+    public:
+    int IntegerMember;
+    float FloatMember;
+    bool BooleanMember;
+
+    int CoolFunc(int a, int b, int c) { 
+        return a + b + c;
+    }
+};
+
+REFLECTA_BEGIN(MyTestClass)
+    REFLECTA_REFLECT_MEMBER(IntegerMember)
+    REFLECTA_REFLECT_MEMBER(FloatMember)
+    REFLECTA_REFLECT_MEMBER(BooleanMember)
+REFLECTA_END(MyTestClass)
+
+TEST_CASE("Ensure that REFLECTA_OFFSETOF works") {
+    struct POD {
+        int A;
+        float B;
+        char C;
+        double D;
+    };
+
+    CHECK(REFLECTA_OFFSETOF(POD, A) == offsetof(POD, A));
+    CHECK(REFLECTA_OFFSETOF(POD, B) == offsetof(POD, B));
+    CHECK(REFLECTA_OFFSETOF(POD, C) == offsetof(POD, C));
+    CHECK(REFLECTA_OFFSETOF(POD, D) == offsetof(POD, D));
+}
+
+TEST_CASE("Ensure that class descriptors exist") {
+    auto class_instance = std::make_unique<MyTestClass>();
+    auto descriptor = class_instance->get_meta_descriptor();
+    CHECK(descriptor != nullptr);
+}
+
+TEST_CASE("Ensuring that properties are correctly reflected") {
+    auto class_instance = std::make_unique<MyTestClass>();
+    auto descriptor = class_instance->get_meta_descriptor();
+    CHECK(descriptor->get_methods().size() == 0);
+    CHECK(descriptor->get_instance_members().size() == 3);
+    CHECK(descriptor->get_property("IntegerMember")->offset_in_class != 0);
+    CHECK(descriptor->get_property("IntegerMember")->descriptor == Reflecta::get_meta_descriptor<int>());
+}
+
+TEST_CASE("Ensuring that property setter and getters work") {
+    auto class_instance = std::make_unique<MyTestClass>();
+    auto descriptor = class_instance->get_meta_descriptor();
+}
